c720d2ac0fa07a0d0378dd4603c62628345a940a
0nepeop1e <engshun.lim@gmail.com>
2023-11-17 18:30:12 +0000

    serve static with stream

--- packages/core/lib/static.ts
+++ packages/core/lib/static.ts
@@ -3,7 +3,8 @@ import type { MiddlewareFunction } from './router';
 import { key } from './context';
 import { resolve } from 'path';
 import { stat, exists } from 'fs/promises';
-import { readFile } from 'fs/promises';
+import { createReadStream } from 'fs';
+import { Readable } from 'stream';
 
 export interface StaticMiddlewareOptions {
     exclude?: string | string[];
@@ -21,23 +22,25 @@ export function staticFiles(
             ? options.exclude
             : [];
     const ignored = new Set<string>();
-    const cacheHeaders = new Map<string, Headers>();
+    const cachedHeaders = new Map<string, Headers>();
     return async (ctx, next) => {
         const req = ctx.get(key.request);
         const url = new URL(req.url);
         const pathname = decodeURI(url.pathname).replace(/^\//, '');
         const path = resolve(root, pathname);
-        if (!(await exists(path))) return next();
-        const file = await stat(path);
-        if (!file.isFile()) return next();
-        if (ignored.has(pathname)) return next();
-        for (const pattern of ignoreList) {
-            if (minimatch(pathname, pattern)) {
-                ignored.add(pathname);
-                return next();
+        if (!cachedHeaders.has(path)) {
+            if (!(await exists(path))) return next();
+            const file = await stat(path);
+            if (!file.isFile()) return next();
+            if (ignored.has(pathname)) return next();
+
+            for (const pattern of ignoreList) {
+                if (minimatch(pathname, pattern)) {
+                    ignored.add(pathname);
+                    return next();
+                }
             }
-        }
-        if (!cacheHeaders.has(path)) {
+
             const headers = new Headers(options.headers);
             if (file.mtimeMs) {
                 headers.set('Last-Modified', new Date(file.mtimeMs).toUTCString());
@@ -46,14 +49,23 @@ export function staticFiles(
                 headers.set('Content-Length', file.size.toString());
             }
             headers.set('Content-Type', Bun.file(path).type);
-            cacheHeaders.set(path, headers);
+            cachedHeaders.set(path, headers);
         }
+        const headers = cachedHeaders.get(path)!;
+
         if (req.headers.has('if-modified-since')) {
             const ifModifiedSince = new Date(req.headers.get('if-modified-since')!);
-            if (ifModifiedSince >= file.mtime) {
-                return new Response(null, { status: 304, headers: cacheHeaders.get(path) });
+            const mtime = new Date(headers.get('Last-Modified')!);
+            if (ifModifiedSince >= mtime) {
+                return new Response(null, { status: 304, headers });
             }
         }
-        return new Response(await readFile(path, 'buffer'), { headers: cacheHeaders.get(path) });
+
+        // Bun.file is not stable, so we use fs.createReadStream instead
+        // TODO: use Bun.file when it's stable
+        const stream = createReadStream(path);
+        return new Response(Readable.toWeb(stream) as ReadableStream, {
+            headers,
+        });
     };
 }
